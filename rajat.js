1539e4b2bf48f9cf5ea0ce64eda5e6644ed26c75
rajatjha9058 <rajatjha20070@gmail.com>
2024-08-09 21:00:53 +0530

    clock

--- rajat.js
+++ rajat.js
@@ -7,35 +7,43 @@
 //  console.log (call1(3,call2))
 
 
-let obj1={
-    name:"rajat",
-    age:17,
-    education:{
-        class:"Bca",
-        year:"1st year"
-    }
+// let obj1={
+//     name:"rajat",
+//     age:17,
+//     education:{
+//         class:"Bca",
+//         year:"1st year"
+//     }
+// }
+
+//  //let obj2=obj1
+//  //let obj2=Object.assign({},obj1);
+//  //let obj2={...obj1}
+
+
+// let obj2=JSON.stringify(obj1)
+
+// let obj3=JSON.parse(obj2)
+
+
+// obj3. name="tushar"
+//  obj3.education.class="B.tech 2nd Semester";
+//  console.log("Orginal object", obj1);
+//  console.log("Copy object", obj2);
+//  console.log("object 3",obj3);
+
+
+function clock() {
+    setInterval(() => {
+        const date = new Date();
+        console.log("number", date);
+
+        console.log("Date==>", date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear());
+        console.log("time==>", date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
+
+    }, 1000);
 }
-
- //let obj2=obj1
- //let obj2=Object.assign({},obj1);
- //let obj2={...obj1}
-
-
-let obj2=JSON.stringify(obj1)
-
-let obj3=JSON.parse(obj2)
-
-
-obj3. name="tushar"
- obj3.education.class="B.tech 2nd Semester";
- console.log("Orginal object", obj1);
- console.log("Copy object", obj2);
- console.log("object 3",obj3);
-
-
-
-
-
+clock()
 
 
 
