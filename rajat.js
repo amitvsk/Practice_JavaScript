90596593ca767fc92428bbaa47ade902dd496103
rajatjha9058 <rajatjha20070@gmail.com>
2024-08-14 10:44:21 +0530

    arrey

--- rajat.js
+++ rajat.js
@@ -94,30 +94,77 @@
 // }
 // timeCounter()
 
-function stopClock(time){
-    let seconed=0;
+// function stopClock(time){
+//     let seconed=0;
     
-    let timec=setInterval(() => {
-        seconed=seconed+1
-        //console.log("minutes",seconed/60);
-        //console.log(("seconed",seconed%60));
-        console.log("timecounter",parseInt(seconed/60)+":"+ seconed%60);
+//     let timec=setInterval(() => {
+//         seconed=seconed+1
+//         //console.log("minutes",seconed/60);
+//         //console.log(("seconed",seconed%60));
+//         console.log("timecounter",parseInt(seconed/60)+":"+ seconed%60);
         
-        if(parseInt(seconed)==time) {
-clearInterval(timec)
-        }  
-    }, 1000);
-    setTimeout(() => {
-        clearInterval(timec)
-    },time*1000*60 );
-}
+//         if(parseInt(seconed)==time) {
+// clearInterval(timec)
+//         }  
+//     }, 1000);
+//     setTimeout(() => {
+//         clearInterval(timec)
+//     },time*1000*60 );
+// }
 
-stopClock(20)
+// stopClock(20)
+
+// function add(){
+//     var count =0;
+//     return{
+// increment:function(value){
+//     count =count+value
+//     return count
+// },
+// dicrement:function(value){
+// count =count-value
+// return count
+// }
+//     }
+// }
+
+
+// let arr=[8,3,5,4,2]
+
+//firstquestion
+// console.log(arr.at(3));
 
 
 
+// seconedquestion
+// console.log(arr.at(0));
 
+// thirdquestion
+// let a=arr.findindex((ele)=>ele==5)
+// console.log(a);
+// let b=arr.findLast((ele)=>ele<5)
+// console.log(b);
 
+// fourthquestion
+// let c=arr.findIndex((ele)=>ele==3)
+// console.log(c);
+
+// sixquestion
+// let d=arr.filter((ele)=>ele%2!==0)
+// console.log(d);
+
+//sevenquestion
+// let e=arr.filter((ele)=>ele>3&&ele<5)
+// console.log(e);
+
+// eightquestion
+// let arr2=[7,9]
+// console.log(arr.concat(arr2));
+
+// fivequestion
+let arr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+let primeNumbers=arr.filter((ele)=>ele==arr)
+console.log(primeNumbers);
 
 
 
